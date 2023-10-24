bf5b5ff00ec4d3a0941528ef4462e42248c6c6d2
NduatiMichael <mknduati@gmail.com>
2023-10-24 12:29:01 +0300

    More data Structures

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_list = []
+    result = 0
+    for i in my_list:
+        if i not in unique_list:
+            unique_list.append(i)
+            result += i
+    return result
